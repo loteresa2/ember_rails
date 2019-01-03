c737d026980d44c1498c2e1653042ad63465e148
teresa-lo <teresa@financeit.io>
2019-01-03 10:49:23 -0500

    ember routes basic- added a multiple authors

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,9 +2,15 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return {
+    return [
+      {
       first: "J.K.",
       last: "Rowling"
-    };
+      },
+      {
+        first: "Tom",
+        last: "Clancy"
+      }
+    ];
   }
 });
