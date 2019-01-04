571c40aebcd2d40b376ec21f95af94f27a572d4a
teresa-lo <teresa@financeit.io>
2019-01-03 21:26:42 -0500

    ember data create record

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,15 +1,16 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  reset(){
-    this.setProperties({
-      first: '',
-      last: ''
-    });
-  },
   actions: {
-    changeFirst(value) {
-      this.set('first', value);
+    saveAuthor(ev){
+      ev.preventDefault();
+      let author = this.store.createRecord('author', this.model);
+      author.save()
+        .then(() => {
+            this.transitionToRoute('author');
+          });
+
+      this.transitionToRoute('author');
     }
   }
 });
--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,11 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // resetController(controller){
-  //   this._super(...arguments);
-  //   controller.reset();
-  // },
-
   model(){
     return {
       first: '',
