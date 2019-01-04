ae07dc63eec17a26ce3cd4f2cb32b1fe21a62a14
teresa-lo <teresa@financeit.io>
2019-01-04 14:06:01 -0500

    Route Query Params: make the search bar work

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,14 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(){
-    // /authors?filter[query]=J
-    return this.store.query('author', {
-      filter: {
-        query: 'J'
-      }
-    });
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+  model({ search }){
+    if(search) {
+      // /authors?filter[query]=J
+      return this.store.query('author', {
+        filter: {
+          query: search
+        }
+      });
+    }
 
-    //return this.store.findAll('author')
+    return this.store.findAll('author')
   }
 });
