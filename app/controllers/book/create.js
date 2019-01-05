2b8163d08c74d444c9bc89fa645bf2f25369409a
teresa-lo <teresa@financeit.io>
2019-01-04 22:18:17 -0500

    Ember power select searching

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -4,6 +4,9 @@ export default Controller.extend({
   actions:{
     selectAuthor(author){
       console.log(author)
+    },
+    searchAuthor(query){
+      return this.store.query('author', {filter: { query } });
     }
   }
 });
