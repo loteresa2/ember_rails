9b9759e013bc7402eeaed04d9a40b5231fd471ec
teresa-lo <teresa@financeit.io>
2019-01-04 22:38:54 -0500

    Ember Save Related

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -2,11 +2,18 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions:{
-    selectAuthor(author){
-      console.log(author)
-    },
     searchAuthor(query){
       return this.store.query('author', {filter: { query } });
+    },
+    saveBook(ev){
+      ev.preventDefault();
+
+      const book = this.store.createRecord('book', this.model);
+
+      book.save()
+        .then(() => {
+            this.transitionToRoute('book')
+          });
     }
   }
 });
