2fcea69a20bd8235295aa6af516c334fe883ec82
teresa-lo <teresa@financeit.io>
2019-01-06 13:36:46 -0500

    Ember Liquid Fire Explore Transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,5 +1,11 @@
 const duration = 600;
-
+const slidingCardIn = [
+  { pickNew: ".slide-out-bg", use: ["fade", { duration }] },
+  {
+    pickNew: ".slide-out-card",
+    use: ["wait", duration, { then: "toLeft" }, { duration }]
+  }
+];
 const slidingCardOut = [
   { pickOld: ".slide-out-bg", use: ["fade", { duration }] },
   {
@@ -7,44 +13,32 @@ const slidingCardOut = [
     use: ["wait", duration, { then: "toRight" }, { duration }]
   }
 ];
-export default function(){
+
+export default function() {
   // Add your transitions here, like:
-  //   this.transition(
-  //     this.fromRoute('people.index'),
-  //     this.toRoute('people.detail'),
-  //     this.use('toLeft'),
-  //     this.reverse('toRight')
-  //   );
+  this.transition(this.hasClass("modal-if"), this.use("fade", { duration }));
 
   this.transition(
-    this.hasClass('modal-if'),
-    this.use('fade', { duration })
+    this.fromRoute("book.detail.index"),
+    this.use("toLeft", { duration }),
+    this.reverse("toRight", { duration })
   );
 
   this.transition(
-    this.fromRoute('book.detail.index'),
-    this.use('toLeft', { duration }),
-    this.reverse('toRight')
+    this.fromRoute("author.detail.index"),
+    this.use("fade", { duration }),
+    this.reverse("fade", { duration })
   );
 
   this.transition(
-    this.fromRoute('author.detail.index'),
-    this.use('fade', { duration }),
-    this.reverse('fade', { duration })
+    this.fromRoute("author.index"),
+    this.use("explode", ...slidingCardIn),
+    this.reverse("explode", ...slidingCardOut)
   );
 
   this.transition(
-    this.fromRoute('author.index'),
-    this.use('explode',
-      {
-        pickNew: '.slide-out-bg',
-        use: ['fade', { duration }]
-      },
-      {
-        pickNew: '.slide-out-card',
-        use: ['toLeft', { duration }]
-      }
-    ),
-    this.reverse('explode', ...slidingCardOut)
+    this.fromRoute("book.index"),
+    this.use("explode", ...slidingCardIn),
+    this.reverse("explode", ...slidingCardOut)
   );
 }
