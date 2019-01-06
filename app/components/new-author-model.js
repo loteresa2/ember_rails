8d4afeec4432b04fa13b6c641e9735a5b98873c9
teresa-lo <teresa@financeit.io>
2019-01-05 19:49:52 -0500

    ember data and component: create author in the modal

--- app/components/new-author-model.js
+++ app/components/new-author-model.js
@@ -1,6 +1,8 @@
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
+  store: service('store'),
   init(){
     this._super(...arguments);
 
@@ -8,5 +10,15 @@ export default Component.extend({
       first: '',
       last: ''
     });
+  },
+
+  actions: {
+    save(ev) {
+      ev.preventDefault();
+      let author = this.get('store').createRecord('author', this.author);
+      author.save().then(() => {
+        this.set('showModal', false);
+      });
+    }
   }
 });
